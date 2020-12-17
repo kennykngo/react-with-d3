5fbdff5f24181f86376786083fdf8737d9492dc2
Kenny Ngo <kenny.ngo.design@gmail.com>
2020-12-16 21:50:27 -0800

    added line and curved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { select } from "d3";
+import { curveCardinal, line, select } from "d3";
 import React, { useEffect, useRef, useState } from "react";
 
 import "./App.css";
@@ -6,13 +6,17 @@ import "./App.css";
 // const data = [25, 30, 45, 60, 20];
 
 function App() {
-  const [data, setData] = useState([25, 30, 45, 60, 20]);
+  const [data, setData] = useState([25, 30, 45, 60, 20, 65, 75]);
   const svgRef = useRef();
 
   // best time to access the svg DOM elemenet is in the useEffect hook
   useEffect(
     () => {
       const svg = select(svgRef.current);
+      const myLine = line()
+        .x((value, index) => index * 50)
+        .y((value) => 150 - value)
+        .curve(curveCardinal);
       // selects all of the existing circle elements AND synchronize them with the data
       // enter new data represented in SVG
       // join creates the new circles
@@ -20,23 +24,30 @@ function App() {
       // update -> updates the circles with a class of updated
       ////// If there's a pre-existing circle element, the update will reuse the circle as part of d3 and update with an updated class
       // If 6 circles exist, will remove one and re-use 6 since there are 5 elements in the data array
+      // svg
+      //   .selectAll("circle")
+      //   .data(data)
+      //   .join(
+      //     // works even if you take out all callback functions and replace with just "circle"
+      //     // join method will create and append the circles for you
+      //     (enter) => enter.append("circle"),
+      //     (update) => update.attr("class", "updated"),
+      //     // exit callback is default
+      //     (exit) => exit.remove()
+      //   )
+      //   // r is radius
+      //   // by placing it at the end after join, the styles are applied to both enter and update
+      //   .attr("r", (value) => value)
+      //   .attr("cx", (value) => value * 2)
+      //   .attr("cy", (value) => value * 2)
+      //   .attr("stroke", "red");
       svg
-        .selectAll("circle")
-        .data(data)
-        .join(
-          // works even if you take out all callback functions and replace with just "circle"
-          // join method will create and append the circles for you
-          (enter) => enter.append("circle"),
-          (update) => update.attr("class", "updated"),
-          // exit callback is default
-          (exit) => exit.remove()
-        )
-        // r is radius
-        // by placing it at the end after join, the styles are applied to both enter and update
-        .attr("r", (value) => value)
-        .attr("cx", (value) => value * 2)
-        .attr("cy", (value) => value * 2)
-        .attr("stroke", "red");
+        .selectAll("path")
+        .data([data])
+        .join("path")
+        .attr("d", (value) => myLine(value))
+        .attr("fill", "none")
+        .attr("stroke", "blue");
     },
     // empty dependency array results in the useEffect being only called once (once the DOM elements have been rendered)
     [data]
